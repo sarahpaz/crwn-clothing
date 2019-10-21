f665b2ee7407ff62a16db25c4af3f55256a58187
Sarah Paz <sarah.paz@gmail.com>
2019-10-20 23:49:39 -0400

    import CollectionPreview component and map through collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data.jsx";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -11,7 +12,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
