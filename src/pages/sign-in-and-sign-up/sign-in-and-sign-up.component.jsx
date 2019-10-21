64eb853a68127b178b8ec05f849d73b1c2f7d9ee
Sarah Paz <sarah.paz@gmail.com>
2019-10-21 19:54:21 -0400

    import SignIn component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import SignIn from "../../components/sign-in/sign-in.component";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInSignUpPage = () => (
-  <div className="sign-in-and-sign-up">SIGN IN</div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInSignUpPage;
