a4a7887f297cd8c1b7048e8e6129de02335eba97
Sarah Paz <sarah.paz@gmail.com>
2020-04-10 01:09:47 -0400

    create sign-up component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
